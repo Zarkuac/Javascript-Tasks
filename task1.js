91d6052c5b2b50d3db8d4624aa6829a80f2893ec
Tsotne Zarkua <102588746+Zarkuac@users.noreply.github.com>
2024-09-11 17:10:56 +0400

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,9 @@
+/* Write a JavaScript function that reverses a number.
+   Example x = 32243;
+   Expected Output: 34223
+*/
+
+
 function numberReverser(number) {
     let result = parseInt(number.toString().split('').reverse().join(''));
     console.log(result);
